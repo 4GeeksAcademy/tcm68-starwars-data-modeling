cde57fa2ae3a40f5951ff19e558c65b298bcad97
Tcm68 <131905512+Tcm68@users.noreply.github.com>
2023-09-22 14:07:50 +0000

    done

--- src/models.py
+++ src/models.py
@@ -7,23 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    species = Column(String(250))
+    homeworld = Column(String(250))
+    description = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    population = Column(Integer)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    capacity = Column(Integer)
+    speed = Column(Integer, nullable=False)
+    length = Column(Integer, nullable=False)
+    weight = Column(Integer, nullable=False)    
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    user = relationship(User)
+    character = relationship(Character)
+    planet = relationship(Planet)
+    vehicle = relationship(Vehicle)
 
     def to_dict(self):
         return {}
